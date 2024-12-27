81a2f8ba4091c3e8091c9279590defadac0ba6bc
AdeeshaNanayakkara <adeeshananayakkara27@gmail.com>
2024-12-27 12:28:22 +0530

    file structure

---
+++ src/test/java/com/example/eventspherebackend/EventSphereBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eventspherebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventSphereBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
