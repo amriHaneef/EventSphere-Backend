b19757149c623a0d97e9d3dc0f8709d3464de054
shanindeewa <139702069+ShanIndeewa@users.noreply.github.com>
2025-01-17 19:38:55 +0530

    done.

--- src/main/java/com/example/eventspherebackend/controller/UserController.java
+++ src/main/java/com/example/eventspherebackend/controller/UserController.java
@@ -41,7 +41,7 @@ public class UserController {
         }
 
         // Return the generated token
-        return ResponseEntity.ok(Map.of("token:", token + " role:" + jwtUtil.getRoleFromToken(token)));
+        return ResponseEntity.ok(Map.of("token:", token));
     }
 
     @PostMapping("/register")
